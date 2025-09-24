9c06cf390a31aa0162fcf2156a53a356c7def7f2
Laurence3123 <thamsanqakatleho645@gmail.com>
2025-09-24 08:47:13 +0200

    Connecting database & referencing models on Data DbContext.

--- Task Management/Data/TaskDbContext.cs
+++ Task Management/Data/TaskDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace Task_Management.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Task_Management.Models;
+
+namespace Task_Management.Data
 {
     public class TaskDbContext
     {
+        public class DatabaseContext : DbContext
+        {
+            public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+            {
+
+            }
+            public DbSet<User> Users { get; set; }
+            public DbSet<TaskItem> Tasks { get; set; }
+        }
     }
 }
